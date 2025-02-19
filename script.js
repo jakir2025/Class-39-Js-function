a5120d37d95973faae75dd4af58c9f6882ca8aec
jakir9324 <jakirhusen9324@gmail.com>
2025-02-19 14:10:52 +0600

    js,function

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+function sub(number1, number2) {
+    let digit1 = number1;
+    let digit2 = number2;
+    
+    let result = digit1-digit2;
+
+    console.log("the sub is ="+result);
+}
+
+function sum(x, y){
+    let digit1 = x;
+    let digit2 = y;
+
+    let result = digit1+digit2;
+
+    console.log("the sum is ="+result);
+}
+
+function mul(x, y) {
+    let multi1 = x;
+    let multi2 = y;
+
+    let result = multi1*multi2;
+
+    console.log("the mul is="+result);
+}
+
+function division(x, y) {
+    let div1 = x;
+    let div2 = y;
+
+    let result = div1/div2;
+
+    console.log("the division is="+result);
+}
+
+console.log("website start");
+console.log("website start");
+console.log("website start");
+console.log("website start");
+console.log("website start");
+console.log("website start");
+console.log("website start");
+console.log("website start");
+console.log("website start");
+
+
+division(150, 20);
+mul(79, 5);
+sum(100, 70);
+sub(90, 60);
+
+
+
